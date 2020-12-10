1332edf69341ee8f3d474c02dc49bc10193b9c42
DiegoLop-Ra <75741402+DiegoLop-Ra@users.noreply.github.com>
2020-12-10 15:44:25 -0600

    se agregan metodos

--- src/Prueba/SeleniumTest.java
+++ src/Prueba/SeleniumTest.java
@@ -1,4 +1,21 @@
 package Prueba;
 
+import org.junit.jupiter.api.beforeAll;
+
 public class SeleniumTest {
+
+    @BeforeAll
+    public void @BeforeAll(){
+
+    }
+
+    @AfterAll
+    public void afterAll(){
+
+    }
+
+    @Test
+    public void openThePageandCheckTheTitle(){
+
+    }
 }
